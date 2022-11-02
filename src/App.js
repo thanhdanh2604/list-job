e99350078d9e675b277aae32fa42db0017a9372b
thanhdanh2604 <33141746+thanhdanh2604@users.noreply.github.com>
2022-11-03 01:28:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import Card from './Compoments/BasicCard.js';
+import AppBar from './Compoments/AppBar.js'
+
+import jobs from './Compoments/data/jobs.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar />
+      {jobs.slice(0,4).map(job=>(
+         <Card title={job.title} salaryMax={job.salaryHigh} city={job.city} />
+      ))}
     </div>
   );
+
 }
 
-export default App;
+export default App
